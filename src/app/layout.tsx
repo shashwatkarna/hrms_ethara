2f837c699abae3a7d3a7b79d476bd3ef54c79309
Shashwat Karna <shashwatkarna6@gmail.com>
2026-02-10 23:40:57 +0530

    db issue fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 import { Navbar } from "@/components/Navbar";
 
@@ -32,6 +33,7 @@ export default function RootLayout({
         <main className="flex-1 bg-muted/10">
           {children}
         </main>
+        <Analytics />
       </body>
     </html>
   );
